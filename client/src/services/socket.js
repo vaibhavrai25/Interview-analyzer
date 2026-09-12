68391421b12502119f7fa1747a7ca4b2f44864b6
Vaibhav Rai <vaibhavrai.143361karnai@gmail.com>
2026-09-13 01:57:08 +0530

    chore: point frontend to production render backend

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://127.0.0.1:8000";
+const SOCKET_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
